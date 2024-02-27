183a34e291abd452c582ecbac81d0b82ff56ec00
deepakmaretha <deepakmaretha6@gmail.com>
2024-02-28 00:25:31 +0530

    Added tfvars to use secrets as environment

--- variable.tf
+++ variable.tf
@@ -1,12 +1,3 @@
-variable "region" {
-    default = "ap-south-1"  
-}
-
-variable "access_key" {
-    default = "AKIA344Z2RINHFVS7ZR3"
-}
-
-variable "secret_key" {
-    default = "TS94vCkN6W5FYdhHbVNE36wYgBEm16obol/IIWc6"
-}
-
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {}
